455f00c9220aea010ca9cf3190a3e13f0fd90e4a
rankopolat <rankokasi@gmail.com>
2023-08-12 13:24:23 +1000

    completed a working database with a user login and register both accepting inputs and checking within database for validation

--- FlyDreamAir/main.py
+++ FlyDreamAir/main.py
@@ -6,10 +6,9 @@ print("Group Project")
 
 
 def register_user():
-    new_username = rName_entry.get()
 
     # Check if the username already exists
-    cursor = Database.conn.execute("SELECT COUNT(*) FROM USER WHERE USERNAME = ?", (rName_entry.get()))
+    cursor = Database.conn.execute("SELECT COUNT(*) FROM USER WHERE USERNAME = ?", (rName_entry.get(),))
     exists = cursor.fetchone()[0]
 
     if exists > 0:
@@ -27,11 +26,10 @@ def register_user():
 
 def register_user_window():
 
-    global rWindow
+    global rWindow,rName_entry, rEmail_entry, rPass_entry, rFrame
+
     rWindow = tk.Toplevel(win)
     rWindow.geometry("400x300")
-
-    global rName_entry, rEmail_entry, rPass_entry, rFrame
 
     rFrame = tk.Frame(rWindow)
     rFrame.pack(pady=20)
@@ -55,48 +53,92 @@ def register_user_window():
     r_button.grid(row=6, columnspan=2, pady=20)
 
 def login_user_window():
-    rWindow.destroy()
 
-win = tk.Tk()
-win.title("Fly Dream Air")
-win.geometry("600x400")
+    global lWindow,lName_entry,lPass_entry,lFrame,exist_label
 
-# Load the image
-bgimg = tk.PhotoImage(file="p.png")
+    lWindow = tk.Toplevel(win)
+    lWindow.geometry("400x300")
 
-# Create a canvas for the background image
-canvas = tk.Canvas(win, width=600, height=400)
-canvas.pack()
+    lFrame = tk.Frame(lWindow)
+    lFrame.pack(pady=20)
 
-bg_label = tk.Label(canvas, image=bgimg)
-bg_label.place(relwidth=1, relheight=1)
+    lName_label = tk.Label(lFrame, text="Username:")
+    lName_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+    lName_entry = tk.Entry(lFrame)
+    lName_entry.grid(row=0, column=1, padx=10, pady=10)
 
-# Calculate the space between buttons
-button_space = 50  # Adjust this value as needed
+    lPass_label = tk.Label(lFrame, text="Password:")
+    lPass_label.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+    lPass_entry = tk.Entry(lFrame)
+    lPass_entry.grid(row=4, column=1, padx=10, pady=10)
 
-# Calculate button width and height
-button_width = 100
-button_height = 40
+    l_button = tk.Button(lFrame, text="Login", command=login_user)
+    l_button.grid(row=6, columnspan=2, pady=20)
 
-# Calculate total width of buttons including space
-total_button_width = 2 * button_width + button_space
+    exist_label = tk.Label(lFrame, text="")
+    exist_label.grid(row=10, columnspan=2, pady=20)
 
-# Calculate the starting x-coordinate for the first button
-start_x = (600 - total_button_width) / 2
+def login_user():
 
-# Make the Register button
-register_button = tk.Button(canvas, text="Register", command=register_user_window, width=15, height=3)
-canvas.create_window(start_x, 200, anchor="nw", window=register_button)
+    username = lName_entry.get()
+    loginCursor = Database.conn.execute("SELECT USERNAME,PASSWORD FROM USER WHERE USERNAME = ?", (username,))
+    exists = loginCursor.fetchone()
 
-# Make the Login button
-login_button = tk.Button(canvas, text="Login", command=login_user_window, width=15, height=3)
-canvas.create_window(start_x + button_width + button_space, 200, anchor="nw", window=login_button)
+    if exists is None:
 
-win.mainloop()
+        exist_label.config(text="username or password is incorrect")
+
+    elif exists[0] == username and exists[1] == lPass_entry.get():
+        exist_label.config(text="Login successful")
+
+    else:
+        exist_label.config( text="username or password is incorrect")
+
+def mainWindow():
+    global win
+    win = tk.Tk()
+    win.title("Fly Dream Air")
+    win.geometry("600x400")
+
+    # Load the image
+    bgimg = tk.PhotoImage(file="p.png")
+
+    # Create a canvas for the background image
+    canvas = tk.Canvas(win, width=600, height=400)
+    canvas.pack()
+
+    bg_label = tk.Label(canvas, image=bgimg)
+    bg_label.place(relwidth=1, relheight=1)
+
+    # Calculate the space between buttons
+    button_space = 50  # Adjust this value as needed
+
+    # Calculate button width and height
+    button_width = 100
+
+    # Calculate total width of buttons including space
+    total_button_width = 2 * button_width + button_space
+
+    # Calculate the starting x-coordinate for the first button
+    start_x = (600 - total_button_width) / 2
+
+    # Make the Register button
+    register_button = tk.Button(canvas, text="Register", command=register_user_window, width=15, height=3)
+    canvas.create_window(start_x, 180, anchor="nw", window=register_button)
+
+    # Make the Login button
+    login_button = tk.Button(canvas, text="Login", command=login_user_window, width=15, height=3)
+    canvas.create_window(start_x + button_width + button_space, 180, anchor="nw", window=login_button)
+
+    win.mainloop()
 
 
-# Create the main window
+def main():
 
+    mainWindow()
+
+if __name__ == "__main__":
+    main()
 
 
 '''Database.conn.execute("INSERT INTO USER (ID,NAME,AGE) \
